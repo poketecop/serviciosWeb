7d3be57b89118d2204e1a579e23a045e86e8a496
Jorge Alvarez Ibanyez <jorgeai93b@gmail.com>
2023-12-16 12:39:38 +0100

    Añade el install de SignalGenerator para que mvn dependency:copy-dependencies funcione correctamente

--- Jorge_Alvarez_Ibaniez/dependencies.bat
+++ Jorge_Alvarez_Ibaniez/dependencies.bat
@@ -1,2 +1,12 @@
-mvn dependency:copy-dependencies
+@echo off
+echo Ejecutando el primer comando...
+call mvn install:install-file -Dfile=lib/SignalGenerator-1.0.jar -DgroupId=es.uned.scc.grados.appdist.trabajos.ws -DartifactId=SignalGenerator -Dversion=1.0 -DgeneratePom=true -Dpackaging=jar
 
+echo.
+
+echo Ejecutando el segundo comando...
+
+call mvn dependency:copy-dependencies
+
+echo.
+echo Proceso completado.
